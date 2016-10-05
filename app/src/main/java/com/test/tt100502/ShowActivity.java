5b07358eee010a5d49c167255fc70f5dd82b97ff
Shaojung <shaojung@gmail.com>
2016-10-05 12:07:46 +0800

    接收 Send Text 訊息

--- app/src/main/java/com/test/tt100502/ShowActivity.java
+++ app/src/main/java/com/test/tt100502/ShowActivity.java
@@ -1,7 +1,9 @@
 package com.test.tt100502;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ShowActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ShowActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
+        Intent it = getIntent();
+        String str = it.getStringExtra(Intent.EXTRA_TEXT);
+        TextView tv = (TextView) findViewById(R.id.textView);
+        tv.setText(str);
+
     }
 }
